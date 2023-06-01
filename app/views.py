0b58f7e3a4d582389ff7a521c9f9594015279e6a
SudheerKumar9 <sudheersaanika11@gmail.com>
2023-06-01 20:43:27 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def sudheer(request):
+    return render(request,'sudheer.html')
+def jarvis(request):
+    return render(request,'jarvis.html')
